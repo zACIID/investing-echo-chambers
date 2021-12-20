2cf1b0f0a1a22c733139c6162e44f599d69f91cc
pierluigimarchioro <marchioropierluigi@gmail.com>
2021-12-20 01:28:19 +0100

    Implemented calculation of user sentiment, now need to debug

--- src/data-prep.py
+++ src/data-prep.py
@@ -33,30 +33,14 @@ NO_USER = "N/A"  # used in the responding to column of submission records
 COMMENT_COL = "comment"
 AUTHOR_COL = "author"
 RESPONDING_TO_COL = "responding_to"
+
+USER_COL = "user"
 SENTIMENT_COL = "sentiment_score"
 
 # Get data from up to two months ago
 TWO_MONTHS_AGO = date.today() - timedelta(days=60)
 THRESHOLD_DATE = TWO_MONTHS_AGO
-OUT_FOLDER = "../output"
-
-
-def main():
-    # some settings (including oauth) imported from praw.ini
-    reddit = praw.Reddit(site_name="wsb", requestor_class=LoggingRequestor,
-                         comment_kind=COMMENT_PREFIX, redditor_kind=USER_PREFIX,
-                         submission_kind=SUBMISSION_PREFIX)
-
-    r_wsb = reddit.subreddit("wallstreetbets")
-
-    # Fetch and save comment data
-    wsb_data = get_comment_data(r_wsb)
-    wsb_data.to_csv(f"{OUT_FOLDER}/wsb-data-{datetime.now()}.csv")
-
-    # Calculate and save user sentiment data
-    user_sentiment_data = get_user_sentiment(wsb_data, COMMENT_COL)
-
-    pass
+OUT_FOLDER = "./output"
 
 
 def get_comment_data(subreddit: praw.models.Subreddit) -> pd.DataFrame:
@@ -64,7 +48,7 @@ def get_comment_data(subreddit: praw.models.Subreddit) -> pd.DataFrame:
     try:
         # Create a submission iterator and get submissions from the past two months.
         # From those, extract the comments and put them into a dataframe
-        submissions_iterator = subreddit.stream().submissions()
+        submissions_iterator = subreddit.stream.submissions()
         for submission in submissions_iterator:
             sub_date = datetime.utcfromtimestamp(submission.created_utc)
 
@@ -72,7 +56,10 @@ def get_comment_data(subreddit: praw.models.Subreddit) -> pd.DataFrame:
             THRESHOLD_DATE = datetime.today() - timedelta(days=2)
 
             if sub_date > THRESHOLD_DATE:
-                sub_data = fetch_data_from_submission(submission)
+                sub_data = fetch_data_from_submission(submission,
+                                                      author_col=AUTHOR_COL,
+                                                      comment_col=COMMENT_COL,
+                                                      responding_to_col=RESPONDING_TO_COL)
                 subreddit_data.append(sub_data)
             else:
                 # All the data has been fetched
@@ -83,10 +70,6 @@ def get_comment_data(subreddit: praw.models.Subreddit) -> pd.DataFrame:
         # Make sure to return all the data fetched to this point, so it
         # doesn't have to be downloaded again (takes a long time)
         return subreddit_data
-
-
-# TODO refactoring in due funzioni, una per fetchare dati da submission e costruire network,
-# una che raggruppa commenti per autore presenti nel submission df e calcola sentiment
 
 
 def fetch_data_from_submission(submission: praw.models.Submission,
@@ -124,7 +107,7 @@ def fetch_data_from_submission(submission: praw.models.Submission,
     unix_time_date = submission.created_utc
     creation_date = datetime.utcfromtimestamp(unix_time_date).date()
     print(f"Fetching submission '{submission.id}' data, created at: {creation_date}")
-    print(f"(Fetching date interval: {TWO_MONTHS_AGO} - {date.today()})")
+    print(f"(Date interval: {TWO_MONTHS_AGO} - {date.today()})")
 
     # Treat submission as if it was a regular comment
     authors.append(submission.author.id)
@@ -159,49 +142,59 @@ def fetch_data_from_submission(submission: praw.models.Submission,
         parent_ids.append(parent_id)
         comment_authors[c.id] = author_id
 
-    df_data = {AUTHOR_COL: authors, COMMENT_COL: comments,
-               RESPONDING_TO_COL: [comment_authors[p_id] for p_id in parent_ids]}
+    df_data = {author_col: authors, comment_col: comments,
+               responding_to_col: [comment_authors[p_id] for p_id in parent_ids]}
     return pd.DataFrame(data=df_data)
 
 
 def get_user_sentiment(comment_data: pd.DataFrame,
                        author_col: str, comment_col: str,
-                       sentiment_col: str) -> pd.DataFrame:
+                       user_col: str, sentiment_col: str) -> pd.DataFrame:
     """
     Calculates the sentiment of users based on their comments.
     Returns a dataframe containing all user's sentiment data.
     :param comment_data: dataframe containing comment data (authors, comment texts)
-    :param author_col: name of the column in the provided dataframe that contains
-            the authors of the comments
+    :param author_col: name of the column in the provided dataframe
+            that contains the authors of the comments
     :param comment_col: name of the column in the provided dataframe that contains
             the actual comment text
-    :return: pandas dataframe containing a "user" column and a "sentiment" column
+    :param user_col: name of the column in the output dataframe that contains
+            the id of each user
+    :param sentiment_col: name of the column in the output dataframe that contains
+            the sentiment score for each user
+    :return: pandas dataframe containing user sentiment data
     """
 
     # https://github.com/cjhutto/vaderSentiment#resources-and-dataset-descriptions
     # positive sentiment: compound score >= 0.05
     # neutral sentiment: (compound score > -0.05) and (compound score < 0.05)
     # negative sentiment: compound score <= -0.05
-
     sentiment_analyzer = SentimentIntensityAnalyzer()
     data = comment_data.copy()
 
     # Calculate sentiment score for each comment
-    # #only compound score is necessary because it sums up neu, pos and neg scores.
+    # only compound score is necessary because it sums up neu, pos and neg scores.
     text_data = preprocess_comments(data[comment_col])
     data[sentiment_col] = text_data.apply(lambda x: (sentiment_analyzer.polarity_scores(x))["compound"])
 
-    # TODO prendere solo colonna autori e sentiment e ritornare df
+    # Calculate the sentiment of each user and keep only the necessary columns
     user_sentiment = data.groupby(by=[author_col], as_index=False).mean()
+    user_sentiment = user_sentiment[[author_col, sentiment_col]]
+    user_sentiment = user_sentiment.rename({author_col: user_col}, axis="columns")
 
-
+    return user_sentiment
 
 
 def preprocess_comments(comments: pd.Series) -> pd.Series:
+    """
+    Performs cleaning and preprocessing operations on the provided comments
+    :param comments:
+    :return: preprocessed comments
+    """
     # Set everything to lower case to make comparisons easier
     comments = comments.str.lower()
 
-    # Remove URLS TODO risolvere url e analizzare testo?
+    # Remove urls TODO risolvere url e analizzare testo?
     comments = comments.apply(lambda x: re.sub(r"http\S+", "", x))
 
     # Remove all the special characters (keep only word characters)
@@ -217,4 +210,27 @@ def preprocess_comments(comments: pd.Series) -> pd.Series:
 
 
 if __name__ == "__main__":
-    main()
+    # some settings (including oauth) imported from praw.ini
+    reddit = praw.Reddit(site_name="wsb", requestor_class=LoggingRequestor,
+                         comment_kind=COMMENT_PREFIX, redditor_kind=USER_PREFIX,
+                         submission_kind=SUBMISSION_PREFIX)
+
+    print("--------- Retrieving r/wsb... ---------")
+    r_wsb = reddit.subreddit("wallstreetbets")
+
+    print("--------- Fetching r/wsb comments... ---------")
+    # Fetch and save comment data
+    wsb_data = get_comment_data(r_wsb)
+
+    print("--------- Saving r/wsb comments into csv... ---------")
+    wsb_data.to_csv(f"{OUT_FOLDER}/wsb-2m-data-{datetime.now()}.csv")
+
+    print("--------- Calculating r/wsb users' sentiment... ---------")
+    # Calculate and save user sentiment data
+    user_sentiment_data = get_user_sentiment(wsb_data, author_col=AUTHOR_COL, comment_col=COMMENT_COL,
+                                             user_col=USER_COL, sentiment_col=SENTIMENT_COL)
+
+    print("--------- Saving r/wsb users' sentiment into csv... ---------")
+    user_sentiment_data.to_csv(f"{OUT_FOLDER}/wsb-2m-user-sentiment-{datetime.now()}")
+
+    print("--------- Completed ---------")
